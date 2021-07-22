b8c77d6511f10fd708cc0b7af40c5e8a9e201837
Devin <Devin.j.narula@gmail.com>
2021-07-21 21:21:54 -0400

    Add navbar

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { RiMenu3Fill } from 'react-icons/ri'
+import {Nav, NavLogo, NavbarContainer, MobileIcon, NavMenu, NavItem, NavLink, NavBtn, NavBtnLink} from './NavbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>
+                        Devin
+                    </NavLogo>
+                    <MobileIcon>
+                        <RiMenu3Fill />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLink to="about">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="experience">Experience</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="projects">Projects</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="contact">Contact</NavLink>
+                        </NavItem>
+                        <NavBtn>
+                            <NavBtnLink to='/resume'>Resume</NavBtnLink>
+                        </NavBtn>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
